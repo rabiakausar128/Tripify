d7711b05e42f815297418e315cf7557aca709c9f
rabiakausar128 <66309920+rabiakausar128@users.noreply.github.com>
2020-07-16 22:18:24 +0500

    Add files via upload

---
+++ HomePage/HomePage.js
@@ -0,0 +1,15 @@
+  
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+     
+      
+      <Link to="/lightbox">Test Payment</Link>
+    </div>
+  )
+}
+
+export default HomePage;
