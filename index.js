f3a1312cc54d23ec57ab3e9bd592aeb7a4d38530
rabiakausar128 <66309920+rabiakausar128@users.noreply.github.com>
2020-07-18 21:06:34 +0500

    Scrapper

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+    const cheerio = require('cheerio');
+    const axios= require('axios');
+    var dest = 'islamabad'
+    var Table = require('easy-table');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    axios.get('https://www.accuweather.com/en/pk/islamabad/258278/daily-weather-forecast/258278').then((res)=>{
+
+        
+        const $ = cheerio.load(res.data);
+       
+            const day=$('.dow').text();
+        console.log(day);
+          const date=$('.sub').text();
+          console.log(date);          
+            const HI=$('.high').text();
+            console.log(HI);
+            const low=$('.low').text();
+            console.log(low);
+            const phrase=$('.phrase').text();
+            console.log(phrase);
+           // console.log(date+" "+HI);
+           // data[0]={date,day,HI,low,phrase};
+            //daata[index]={day,date,HI,low,phrase};
+            //console.log(daata);
+        });
+        
+    
+  //  var data[index]=daata[index];
+    
+   
+   // var t = new Table
+     
+  //  data.forEach(function() {
+   //   t.cell('Day',day)
+     // t.cell('date', date)
+      //t.cell('HI temp', HI)
+      //t.cell('Low temp',low)
+      //t.cell('Weather', phrase)
+      //t.newRow()
+    //})
+     
+   // console.log(t.toString())
+
+       
+   
+     
